9c1a7b253564b0da267823e1e9f0e7ad8c04ab0c
aureliapragada <124860750+aurelia12345@users.noreply.github.com>
2023-05-18 18:46:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import argparse
 import mysql.connector
 import requests
+import sys
 
 parser = argparse.ArgumentParser()
 
@@ -35,38 +36,59 @@ params = {
     "lock_on_collection": "false",
     "dimensions": args.DIMENSIONS
 }
+try:
+    # db connection object
+    db = mysql.connector.connect(
+        host=args.DB_HOSTNAME,
+        user=args.DB_USERNAME,
+        password=args.DB_PASSWORD,
+        database=args.DB_NAME
+    )
 
-response = requests.get(api_endpoint, headers=headers, params=params)
-data = response.json()
+except Exception as e:
+    print("An error occured while connecting to database:", e)
+    sys.exit(1)
 
-# db connection object
-db = mysql.connector.connect(
-    host=args.DB_HOSTNAME,
-    user=args.DB_USERNAME,
-    password=args.DB_PASSWORD,
-    database=args.DB_NAME
-)
+try:
+    # making the API request
+    response = requests.get(api_endpoint, headers=headers, params=params)
+    # modifying response to JSON format
+    data = response.json()
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-# Insert data into the SQL database
-data_to_insert = []
-for item in data["connections"]:
-    connection = item["connection"]
-    app = item["app"]
-    platform = item["platform"]
-    country = item["country"]
-    impressions = item["impressions"]
-    ad_revenue = item["ad_revenue"]
-    date = item["date"]
+    # Insert data into the SQL database
+    data_to_insert = []
+    for item in data["connections"]:
+        connection = item["connection"]
+        app = item["app"]
+        platform = item["platform"]
+        country = item["country"]
+        impressions = item["impressions"]
+        ad_revenue = item["ad_revenue"]
+        date = item["date"]
 
-    data_to_insert.append((connection, app, platform, country, impressions, ad_revenue, date))
+        data_to_insert.append((connection, app, platform, country, impressions, ad_revenue, date))
 
-query = "INSERT INTO reports (connection_str, app, platform, country, impressions, ad_revenue, date_field) " \
-        "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    query = "INSERT INTO reports (connection_str, app, platform, country, impressions, ad_revenue, date_field) " \
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
-cursor.executemany(query, data_to_insert)
+    cursor.executemany(query, data_to_insert)
 
-db.commit()
-# close connection to db
-db.close()
+except requests.exceptions.RequestException as e:
+    # Handle connection errors, timeouts, or other request-related exceptions
+    print("An error occurred during the API request:", e)
+
+except ValueError as e:
+    # Handle JSON decoding errors if the response is not in the expected format
+    print("Error decoding JSON response:", e)
+
+except Exception as e:
+    # Handle any other exceptions that may occur
+    print("An unexpected error occurred:", e)
+
+finally:
+    # commit data
+    db.commit()
+    # close connection to db
+    db.close()
